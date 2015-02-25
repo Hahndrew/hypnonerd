8576e28b0c816a6cfafab6e40c700fb8b5522247
hahndrew <andrew.g.hahn@gmail.com>
2015-02-25 08:16:00 -0600

    Initial Commit

---
+++ HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Andrew Hahn on 2/25/15.
+//  Copyright (c) 2015 Andrew Hahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Andrew Hahn on 2/25/15.
+//  Copyright (c) 2015 Andrew Hahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
